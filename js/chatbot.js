5a0fe6fb9b4bec62cfd8f8084971ec6d9398391e
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-19 13:37:53 +0530

    updated

--- js/chatbot.js
+++ js/chatbot.js
@@ -2,15 +2,17 @@ const chatIcon = document.querySelector('.chat-icon');
 const chatBox = document.querySelector('.chat-box');
 
 chatIcon.addEventListener('click', () => {
-  chatBox.classList.toggle('open');
+    chatBox.classList.toggle('open');
 });
 
 
 const botResponse = {
     "hi": "Hello there!",
     "how are you": "I'm doing well, thank you for asking.",
-    "what is your name": "My name is ChatBot.",
-    "default": "I'm sorry, I don't understand what you mean. Can you please rephrase your question?"
+    "what is your name": "I'm Lia and I'm a chatbot.",
+    "please tell me about the packages": "We have a lot of packages in both Goa and Andaman Tour.",
+    "what": "Can you please repeat the question?",
+    "default": "I'm sorry, I don't understand what you mean. Can you please explain it more clearly?"
 };
 
 document.addEventListener("DOMContentLoaded", () => {
