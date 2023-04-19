716b0d4e4c1e3cb712bc43f3a0d78da06e33a9c9
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-19 14:54:32 +0530

    updated

--- js/counter.js
+++ js/counter.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 10000,
+            duration: 8000,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
