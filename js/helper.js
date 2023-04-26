7946a4d50d7724f8a7b8d1f7786745f3639528c2
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-27 00:14:25 +0530

    updated

--- js/helper.js
+++ js/helper.js
@@ -223,21 +223,3 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// scroll appear logo
-var element = document.querySelector(".my-element");
-element.style.display = "none"; // hide the element on page load
-
-window.addEventListener("scroll", function () {
-  var position = element.getBoundingClientRect();
-
-  // If the element is in view and the user has already scrolled down, show it
-  if (position.top < window.innerHeight && position.bottom >= 0 && window.pageYOffset > 0) {
-    element.style.display = "block";
-  }
-
-  // If the scrollbar is at the top of the page, hide the element
-  if (document.documentElement.scrollTop === 0) {
-    element.style.display = "none";
-  }
-});
-
