7060de566020087da02c01537f5a161cd4118f37
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-27 00:11:32 +0530

    updated

--- js/helper.js
+++ js/helper.js
@@ -207,3 +207,37 @@ var ScrollPosStyler = (function (document, window) {
 
   return pub;
 })(document, window);
+
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".my-element");
+  var position = element.getBoundingClientRect();
+
+  // If the element is in view, show it
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.style.display = "block";
+  }
+
+  // If the scrollbar is at the top of the page, hide the element
+  if (document.documentElement.scrollTop === 0) {
+    element.style.display = "none";
+  }
+});
+
+// scroll appear logo
+var element = document.querySelector(".my-element");
+element.style.display = "none"; // hide the element on page load
+
+window.addEventListener("scroll", function () {
+  var position = element.getBoundingClientRect();
+
+  // If the element is in view and the user has already scrolled down, show it
+  if (position.top < window.innerHeight && position.bottom >= 0 && window.pageYOffset > 0) {
+    element.style.display = "block";
+  }
+
+  // If the scrollbar is at the top of the page, hide the element
+  if (document.documentElement.scrollTop === 0) {
+    element.style.display = "none";
+  }
+});
+
