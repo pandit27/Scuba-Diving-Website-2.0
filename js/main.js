5765a63ee50a60d716e4f7a464d950e035b7d942
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-28 23:09:54 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -93,3 +93,32 @@
 
 
 
+//Offcanvas Menu
+$(".canvas-open").on('click', function () {
+    $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+    $(".offcanvas-menu-overlay").addClass("active");
+});
+
+$(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+    $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+    $(".offcanvas-menu-overlay").removeClass("active");
+});
+
+// Search model
+$('.search-switch').on('click', function () {
+    $('.search-model').fadeIn(400);
+});
+
+$('.search-close-switch').on('click', function () {
+    $('.search-model').fadeOut(400, function () {
+        $('#search-input').val('');
+    });
+});
+
+/*------------------
+    Navigation
+--------------------*/
+$(".mobile-menu").slicknav({
+    prependTo: '#mobile-menu-wrap',
+    allowParentLinks: true
+});
