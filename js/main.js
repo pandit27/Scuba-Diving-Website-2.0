cdf5c3f090ad6b71535c72d29bae9eabc32adbce
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-29 22:55:02 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -91,34 +91,9 @@
 
 })(jQuery);
 
+const closeButton = document.querySelector('.close-btn');
+const navbarCollapse = document.querySelector('.navbar-collapse');
 
-
-//Offcanvas Menu
-$(".canvas-open").on('click', function () {
-    $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
-    $(".offcanvas-menu-overlay").addClass("active");
-});
-
-$(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
-    $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
-    $(".offcanvas-menu-overlay").removeClass("active");
-});
-
-// Search model
-$('.search-switch').on('click', function () {
-    $('.search-model').fadeIn(400);
-});
-
-$('.search-close-switch').on('click', function () {
-    $('.search-model').fadeOut(400, function () {
-        $('#search-input').val('');
-    });
-});
-
-/*------------------
-    Navigation
---------------------*/
-$(".mobile-menu").slicknav({
-    prependTo: '#mobile-menu-wrap',
-    allowParentLinks: true
+closeButton.addEventListener('click', () => {
+    navbarCollapse.classList.remove('show');
 });
